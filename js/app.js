02dd5d52b6a689d94071adb14e2c5a7b52d07800
LadyPi <ms.vanessa.farias.dm@gmail.com>
2016-07-10 20:04:02 -0700

    adds recruiters to list

--- js/app.js
+++ js/app.js
@@ -4,16 +4,40 @@ angular
 
 RecruiterCtrl.$inject = ["$scope", "$firebaseArray"];
 
+
 function RecruiterCtrl($scope, $firebaseArray) {
-	var ref = firebase.database().ref().child("Recruiters");
-	$scope.Recruiters = $firebaseArray(ref);
-	$scope.recruiter = {};
-}
-$scope.addRecruiter = function() {
-	$scope.Recruiters.$add({
+	var ref = firebase.database().ref().child("recruiters");
+	$scope.recruiters = $firebaseArray(ref);
+	$scope.recruiter = [];
+
+	$scope.addRecruiter = function() {
+	$scope.recruiters.$add({
 		name: $scope.recruiter.name,
 		industry: $scope.recruiter.industry
 	});
 	// clear form
-	// $scope.recruiter = {};
+	$scope.recruiter = {};
 };
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $scope.recruiter = "";
+	// var self = this;
+	// self.all = [];
+	// self.addRecruiter = addRecruiter;
+	// self.newRecruiter = {};
+	// self.getRecruiters = getRecruiters;
+	// self.deleteRecruiter = deleteRecruiter;
